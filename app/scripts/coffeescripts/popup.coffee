b8175142b11432fdf4a4f6945d179be1fd6c0639
Kyle Qian <cqian@stanford.edu>
2015-08-09 03:13:30 -0400

    Preliminary Feedly mix integration

--- app/scripts/coffeescripts/popup.coffee
+++ app/scripts/coffeescripts/popup.coffee
@@ -3,6 +3,7 @@ do ($=jQuery) ->
 		url = "https://cloud.feedly.com"
 		auth = "/v3/auth/auth"
 		subs = "/v3/subscriptions"
+		mix = "/v3/mixes/contents?streamId=" + encodeURIComponent("feed/http://feeds.feedburner.com/TechCrunch/")
 		clientid = "sandbox"
 		clientSecret = "YNXZHOH3GPYO6DF7B43K"
 		redirectURI = encodeURI("http://localhost")
@@ -11,12 +12,12 @@ do ($=jQuery) ->
 				Authorization: "OAuth AlmGg7p7ImEiOiJGZWVkbHkgRGV2ZWxvcGVyIiwiZSI6MTQ0Njc1ODQ4MjIwMSwiaSI6IjFlYjk2NjE4LTFmYmItNDUwYS04NjhlLWM1ZDg4MzRjMjBhYiIsInAiOjYsInQiOjEsInYiOiJwcm9kdWN0aW9uIiwidyI6IjIwMTUuMzEiLCJ4Ijoic3RhbmRhcmQifQ:feedlydev"
 			}
 			success: (data, textStatus) ->
-				for entry in data
-					$(".test").append "#{entry.title}<br>"
+				for entry in data.items
+					$(".test").append "#{entry.originId}<br>"
 			# response_type: "code",
 			# client_id: clientid,
 			# redirect_uri: redirectURI,
 			# scope: "https://cloud.feedly.com/subscriptions"
 		}
 		$(window).bind "keydown", "meta+i", (e) ->
-			$.ajax "#{url}#{subs}", params
+			$.ajax "#{url}#{mix}", params
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,10 +2,11 @@
 (function() {
   (function($) {
     return document.addEventListener("DOMContentLoaded", function() {
-      var auth, clientSecret, clientid, params, redirectURI, subs, url;
+      var auth, clientSecret, clientid, mix, params, redirectURI, subs, url;
       url = "https://cloud.feedly.com";
       auth = "/v3/auth/auth";
       subs = "/v3/subscriptions";
+      mix = "/v3/mixes/contents?streamId=" + encodeURIComponent("feed/http://feeds.feedburner.com/TechCrunch/");
       clientid = "sandbox";
       clientSecret = "YNXZHOH3GPYO6DF7B43K";
       redirectURI = encodeURI("http://localhost");
@@ -14,17 +15,18 @@
           Authorization: "OAuth AlmGg7p7ImEiOiJGZWVkbHkgRGV2ZWxvcGVyIiwiZSI6MTQ0Njc1ODQ4MjIwMSwiaSI6IjFlYjk2NjE4LTFmYmItNDUwYS04NjhlLWM1ZDg4MzRjMjBhYiIsInAiOjYsInQiOjEsInYiOiJwcm9kdWN0aW9uIiwidyI6IjIwMTUuMzEiLCJ4Ijoic3RhbmRhcmQifQ:feedlydev"
         },
         success: function(data, textStatus) {
-          var entry, i, len, results;
+          var entry, i, len, ref, results;
+          ref = data.items;
           results = [];
-          for (i = 0, len = data.length; i < len; i++) {
-            entry = data[i];
-            results.push($(".test").append(entry.title + "<br>"));
+          for (i = 0, len = ref.length; i < len; i++) {
+            entry = ref[i];
+            results.push($(".test").append(entry.originId + "<br>"));
           }
           return results;
         }
       };
       return $(window).bind("keydown", "meta+i", function(e) {
-        return $.ajax("" + url + subs, params);
+        return $.ajax("" + url + mix, params);
       });
     });
   })(jQuery);
